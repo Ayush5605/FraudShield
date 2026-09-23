7039d587ec87edffa30b1f938c0577aff086d5e6
Ayush Auti <ayushauti5605@gmail.com>
2026-09-23 16:03:33 +0530

    DTO part for account service done

--- account-service/src/main/java/com/bank/account_service/DTO/AccountResponse.java
+++ account-service/src/main/java/com/bank/account_service/DTO/AccountResponse.java
@@ -1,4 +1,22 @@
 package com.bank.account_service.DTO;
 
+import entity.AccountStatus;
+import entity.AccountType;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class AccountResponse {
+
+    private  String id;
+    private String accountNumber;
+    private String accountHolderName;
+    private String email;
+    private String phone;
+    private AccountType accountType;
+    private AccountStatus status;
+    private BigDecimal balance;
+    private BigDecimal dailyTransactionLimit;
+    private LocalDateTime createdAt;
+
 }
